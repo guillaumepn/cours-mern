579eab86568b3ca58648087780d465f23237f307
guillaumepn <guillaumepn@free.fr>
2019-03-04 14:20:23 +0100

    reorganize files

--- server/index.js
+++ server/index.js
@@ -1,26 +1,6 @@
 const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://mongo', {
-    user: process.env.MONGO_USER,
-    pass: process.env.MONGO_PASS,
-    dbName: process.env.MONGO_DBNAME,
-    useNewUrlParser: true
-});
-
-const db = mongoose.connection;
-
-db.on('error', (error) => console.log(error));
-db.on('open', () => console.log('Connected'));
-
-const Schema = mongoose.Schema;
-
-const movieDetailSchema = new Schema({
-    title: String,
-    year: {type: Number, min: 1900},
-    releaseDate: Date
-})
-
-const MovieDetail = db.model('movieDetail', movieDetailSchema);
+const db = require('./lib/db');
+const MovieDetail = require('./models/movie');
 
 const movie = new MovieDetail();
 movie.title = 'Taxi';
@@ -29,3 +9,5 @@ movie.releaseDate = new Date(1999, 10, 2);
 movie.save()
     .then(() => console.log("saved"))
     .catch(error => console.log(error));
+
+MovieDetail.find().then(items => console.log(items)).catch(error => console.log(error));
